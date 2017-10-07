ebcdcfc19c7de4c201300ca1b3d08dfe93625ada
Chiahao Lin <y80811@gmail.com>
2017-10-08 01:34:02 +0800

    update packages and check redux-extension

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,24 +1,27 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import { createLogger } from 'redux-logger'
-import { routerMiddleware } from 'react-router-redux'
-import reduxThunk from 'redux-thunk'
-import promiseMiddleware from 'redux-promise-middleware'
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createLogger } from 'redux-logger';
+import { routerMiddleware } from 'react-router-redux';
+import reduxThunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise-middleware';
 
-import rootReducer from '../reducers/'
-import history from './history'
+import rootReducer from '../reducers/';
+import history from './history';
 
-const initialState = {}
+const initialState = {};
 
-export default createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(
-      createLogger(),
-      reduxThunk,
-      promiseMiddleware(),
-      routerMiddleware(history)
-    ),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-)
+const middleware = applyMiddleware(
+  createLogger(),
+  reduxThunk,
+  promiseMiddleware(),
+  routerMiddleware(history)
+);
+
+const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__
+  ? compose(
+      middleware,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  : middleware;
+
+export default createStore(rootReducer, initialState, enhancer);
